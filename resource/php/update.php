eaca1a201ccd4ad08168ca68bbf8480388afd35f
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-06-07 23:12:37 +0200

    [Denis Çerri]Add Update Functionality
    The project is split into small chunks for each contributor. My task was Update.\n When other contributors finish their task, i will add a commit with their name in the commit message.\n ToDo: \n - Check if the updated entity is trying to be something that is already in the database. Keys: Name, Surname

--- resource/php/update.php
+++ resource/php/update.php
@@ -1,1 +1,30 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . "/phpcrud/dbconn.php";
+    if(isset($_GET['id'])){
+        $empID = $_GET['id'];
+        $sql = "SELECT * FROM employees WHERE id=".$empID;
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        
+    }else if(isset($_POST["update"])){
+        $id = $_POST['id'];
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $gender = $_POST['gender'];
+        $country = $_POST['country'];
+        $city = $_POST['city'];
+        $position = $_POST['position'];
 
+        $sql= "UPDATE employees SET firstName='$firstName',lastName='$lastName',
+        email='$email',phone='$phone',gender='$gender',country='$country',city='$city',position='$position' where id=".$id;
+        
+        $response = mysqli_query($conn, $sql);
+        if($response){
+            header('Location: ../../index.php?success=SucessfullyUpdatedEmployee');
+        }else{
+            header('Location: ../../index.php?error=FailedUpdatingEmployee');
+        }
+        
+    }
+?>
