ec298eeeb2fc5b83f23682058796cde7c2d500fa
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-06-11 02:33:11 +0200

    Added Javascript function
    Check if the new entity that is being created has all the necessary input fileds properly filled out. Will have to improve this later.
    -ToDo: Search function? inconclusive

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,1 +1,13 @@
+function validateCreation(){
 
+    //check for empty inputs
+    var form = $('form').serializeArray();
+    for(var i = 0; i < form.length; i++){
+        if(form[i].value == ''){
+            alert(`Please fill in the '${form[i].name}' input`);
+            return false;
+        }
+    }
+    
+    return true;
+}
