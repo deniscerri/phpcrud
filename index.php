2b370e125af4efccb428ec66495014a17c8502cc
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-06-11 02:26:27 +0200

    Modified Index.php

--- index.php
+++ index.php
@@ -4,51 +4,54 @@
 <html lang="en">
 <head>
     <title>Employee Management [Dashboard]</title>
+    <link rel="stylesheet" href="resource/css/styles.css">
+    <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 </head>
 <body>
-    <?php if(mysqli_num_rows($result)) {?>
-        <table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Surname</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Gender</th>
-                    <th>Country</th>
-                    <th>City</th>
-                    <th>Position</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    $i = 0; 
-                    while($rows = mysqli_fetch_assoc($result)){
-                        $i++;
-                ?>
+    <div class="container">
+        <?php if(mysqli_num_rows($result)) {?>
+            <table class="table">
+                <thead>
                     <tr>
-                        <td><?php echo $i?></td>
-                        <td><?php echo $rows['firstName']?></td>
-                        <td><?php echo $rows['lastName']?></td>
-                        <td><?php echo $rows['email']?></td>
-                        <td><?php echo $rows['phone']?></td>
-                        <td><?php echo $rows['gender']?></td>
-                        <td><?php echo $rows['country']?></td>
-                        <td><?php echo $rows['city']?></td>
-                        <td><?php echo $rows['position']?></td>
-                        <td>
-                            <?php echo '<a href="views/updateEmployee.php?id='.$rows['id'].'">Update</a>' ?>
-                        </td>
-                        <td>
-                            <?php echo '<a href="resource/php/delete.php?id='.$rows['id'].'">Delete</a>' ?>
-                        </td>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Surname</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Gender</th>
+                        <th>Country</th>
+                        <th>City</th>
+                        <th>Position</th>
                     </tr>
-                <?php }?>
-            </tbody>
-        </table>
-    <?php }else{ echo 'No Employees Recorded.';}?>
-    <br>
-    <a href="views/createEmployee.php"><input type="button" value="Create Employee"></a> <br>
+                </thead>
+                <tbody>
+                    <?php
+                        $i = 0; 
+                        while($rows = mysqli_fetch_assoc($result)){
+                            $i++;
+                    ?>
+                        <tr>
+                            <td><?php echo $i?></td>
+                            <td><?php echo $rows['firstName']?></td>
+                            <td><?php echo $rows['lastName']?></td>
+                            <td><?php echo $rows['email']?></td>
+                            <td><?php echo $rows['phone']?></td>
+                            <td><?php echo $rows['gender']?></td>
+                            <td><?php echo $rows['country']?></td>
+                            <td><?php echo $rows['city']?></td>
+                            <td><?php echo $rows['position']?></td>
+                            <td>
+                                <?php echo '<a href="views/updateEmployee.php?id='.$rows['id'].'"><ion-icon name="create"></ion-icon></a>' ?>
+                            </td>
+                            <td>
+                                <?php echo '<a href="resource/php/delete.php?id='.$rows['id'].'"><ion-icon name="trash"></ion-icon></a>' ?>
+                            </td>
+                        </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        <?php }else{ echo 'No Employees Recorded.';}?> <br>
+        <a href="views/createEmployee.php"><button id="createEmp" type="button" value="Create Employee">Create Employee</button></a> <br>
+    </div>
 </body>
 </html>
