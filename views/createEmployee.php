f460c766b4c32f8f75734267d96ae5b03b751430
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-06-15 17:52:33 +0200

    [Mikel Shevroja] Create Form for Create Employee

--- views/createEmployee.php
+++ views/createEmployee.php
@@ -4,13 +4,35 @@
     <title>Employee Management [Add]</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <label for="firstName">First Name</label>
-        <input type="text" name="firstName" value="Enter your first name">
-        <label for="lastName">Last Name</label>
-        <input type="text" name="lastName" value="Enter your last name">
-        <button type="submit" name="save">Save</button>
-        <!-- add other tags... -->
+    <form action="/resource/php/create.php">
+
+        <label for="emri">Emri:</label><br>
+        <input type="text" id="emri" name="emri" value=""><br>
+
+        <label for="mbiemri">Mbiemri:</label><br>
+        <input type="text" id="mbiemri" name="mbiemri" value=""><br>
+
+        <label for="email">Email:</label><br>
+        <input type="text" id="email" name="email" value=""><br>
+
+        <label for="telefon">Telefon:</label><br>
+        <input type="text" id="telefon" name="telefon" value=""><br>
+
+        <label>Gjinia: </label><br />
+        <input type="radio" name="gjinia" value="mashkull">Mashkull<br />
+        <input type="radio" name="gjinia" value="femer">Femer<br />
+        <input type="radio" name="other" value="other">Other<br />
+
+        <label for="shteti">Shteti:</label><br>
+        <input type="text" id="shteti" name="shteti" value=""><br>
+
+        <label for="qyteti">Qyteti:</label><br>
+        <input type="text" id="qyteti" name="qyteti" value=""><br>
+
+        <label for="pozicioni">Pozicioni:</label><br>
+        <input type="text" id="pozicioni" name="pozicioni" value=""><br>
+
+        <input type="submit" value="Regjistro Punonjes ">
     </form>
 </body>
 </html>
