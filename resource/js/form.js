a9b4f9c0defa610c407bf8315fe5093629258d65
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2021-06-20 00:39:08 +0200

    Add separate js file for createEmployee and updateEmployee
    Alot of elements that are checked in index.js are not present in these pages so to avoid errors, i made a separate file for them.

---
+++ resource/js/form.js
@@ -0,0 +1,12 @@
+function validateCreation(){
+    //check for empty inputs
+    var form = $('form').serializeArray();
+    for(var i = 0; i < form.length; i++){
+        if(form[i].value == ''){
+            alert(`Please fill in the '${form[i].name}' input`);
+            return false;
+        }
+    }
+    
+    return true;
+}
